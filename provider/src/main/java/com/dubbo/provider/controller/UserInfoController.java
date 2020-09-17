a6ab645d1e2530d4503c7f49aed8e3499df53e5f
yuanxin <577518220@qq.com>
2020-09-17 09:46:38 +0800

    mybatis dubbo

---
+++ provider/src/main/java/com/dubbo/provider/controller/UserInfoController.java
@@ -0,0 +1,19 @@
+package com.dubbo.provider.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author yuanxin
+ * @since 2020-09-16
+ */
+@RestController
+@RequestMapping("/provider/user-info")
+public class UserInfoController {
+
+}
